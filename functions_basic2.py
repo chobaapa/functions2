d7e8a5408e5ac75c3c825c6ab7ea74f07536cd05
chobaapa <127275823+chobaapa@users.noreply.github.com>
2023-03-21 00:11:50 -0400

    Update functions_basic2.py

--- functions_basic2.py
+++ functions_basic2.py
@@ -27,17 +27,18 @@ print(first_plus_length([1,2,3,4,5]))
 # Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 # Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 # Example: values_greater_than_second([3]) should return False
+
 def values_greater_than_second(list):
     if len(list) < 2:
         return False
     output = []
     for i in range(0,len(list)):
-        if list[i] > list[i]
+        if list[i] > list[1]:
+            output.append(list[i])
     print(len(output))
     return output
 print (values_greater_than_second([5,2,3,2,1,4]))
 print (values_greater_than_second([3]))
-
 
 # This Length, That Value - Write a function that accepts two integers as parameters: size and value. The function should create and return a list whose length is equal to the given size, and whose values are all the given value.
 # Example: length_and_value(4,7) should return [7,7,7,7]
